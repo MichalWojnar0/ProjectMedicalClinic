e1d9e294de15baf4e5e9feb2598a4c594c202c83
MichalWojnar0 <michal.wojnar@onet.pl>
2024-07-01 06:36:15 +0200

    Dodanie całego appointments

---
+++ ProjectMedicalClinic/Controllers/AppointmentsController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectMedicalClinic.Models;
+using System.Linq;
+
+namespace ProjectMedicalClinic.Controllers
+{
+    public class AppointmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var appointments = AppointmentsRepository.GetAppointments();
+            return View(appointments);
+        }
+
+        public IActionResult Add()
+        {
+            ViewBag.Action = "add";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                AppointmentsRepository.AddAppointment(appointment);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Action = "add";
+            return View(appointment);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var appointment = AppointmentsRepository.GetAppointmentById(id);
+            if (appointment == null)
+            {
+                return NotFound();
+            }
+            ViewBag.Action = "edit";
+            return View(appointment);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Appointment appointment)
+        {
+            if (ModelState.IsValid)
+            {
+                AppointmentsRepository.UpdateAppointment(appointment.AppId, appointment);
+                return RedirectToAction(nameof(Index));
+            }
+            ViewBag.Action = "edit";
+            return View(appointment);
+        }
+        public IActionResult Delete(int id)
+        {
+            AppointmentsRepository.DeleteAppointment(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
